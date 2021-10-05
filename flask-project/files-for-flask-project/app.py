aaa1a59d333cc0917dbc22dbf9f782e40d344069
csulva <chrissulva1@gmail.com>
2021-10-04 21:03:30 -0400

    new edits to the flask project

--- flask-project/files-for-flask-project/app.py
+++ flask-project/files-for-flask-project/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
