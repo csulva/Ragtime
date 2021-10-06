1f0ce9fb9a79c409d2e08abbdcc16c0b09f9f941
csulva <chrissulva1@gmail.com>
2021-10-05 22:19:01 -0400

    New updates to flask app, adding error messages

--- flask-project/files-for-flask-project/app.py
+++ flask-project/files-for-flask-project/app.py
@@ -32,3 +32,23 @@ def square(number):
 @app.route('/derived')
 def derived():
     return render_template('derived.html')
+
+@app.errorhandler(403)
+def forbidden(e):
+    error_title = 'Forbidden'
+    error_msg = 'You shouldn\t be here.'
+    return render_template('error.html', error_title=error_title, error_msg=error_msg), 403
+
+@app.errorhandler(404)
+def page_not_found(e):
+    error_title = 'Not Found'
+    error_msg = 'That page doesn\'t exist.'
+    return render_template('error.html', error_title=error_title, error_msg=error_msg), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    error_title = "Internal Server Error"
+    error_msg = "Sorry, we seem to be experiencing some technical difficulties"
+    return render_template('error.html',
+                           error_title=error_title,
+                           error_msg=error_msg), 500
