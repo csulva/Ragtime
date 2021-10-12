a9844fbea5f0ea7d379ddc548436cd40fb0f352b
csulva <chrissulva1@gmail.com>
2021-10-12 19:14:14 -0400

    Adding sql functionality to the name form

--- flask-project/files-for-flask-project/app.py
+++ flask-project/files-for-flask-project/app.py
@@ -54,10 +54,19 @@ def make_shell_context():
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        session['name'] = form.name.data
+        name_entered = form.name.data
+        user = User.query.filter_by(username=name_entered).first()
+        if user is None:
+            user = User(username=name_entered)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = name_entered
         flash('Great! We hope you enjoy the community')
         return redirect(url_for('index'))
-    return f""" {render_template('index.html', form=form, name=session.get('name'))}
+    return f""" {render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))}
     <p><a href="http://127.0.0.1:5000/songs">Song List</a></p>
     <p><a href="http://127.0.0.1:5000/about">About Me</a></p>
     """
