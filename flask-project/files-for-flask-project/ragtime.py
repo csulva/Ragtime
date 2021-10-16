47f7dc5ff9985752585f2de0d4974c9629d3c415
csulva <chrissulva1@gmail.com>
2021-10-15 20:10:50 -0400

    edits to flask app

--- flask-project/files-for-flask-project/ragtime.py
+++ flask-project/files-for-flask-project/ragtime.py
@@ -7,7 +7,6 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 migrate = Migrate(app, db, render_as_batch=True)
 
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Role=Role, User=User)
