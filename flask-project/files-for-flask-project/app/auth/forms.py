cecfacba99ed86009d010583def1048ec5cddfb7
csulva <chrissulva1@gmail.com>
2021-10-25 21:21:43 -0400

    adding style and functionality for login

--- flask-project/files-for-flask-project/app/auth/forms.py
+++ flask-project/files-for-flask-project/app/auth/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.fields.core import BooleanField
 from wtforms.fields.simple import PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    username = StringField('Username:', validators=[DataRequired()])
+    email = StringField('Email:', validators=[DataRequired(), Length(64), Email()])
     password = PasswordField('Password:', validators=[DataRequired()])
+    remember_me = BooleanField()
     submit = SubmitField('Log In')
--- flask-project/files-for-flask-project/app/auth/views.py
+++ flask-project/files-for-flask-project/app/auth/views.py
@@ -11,20 +11,21 @@ from app.models import User
 def register():
     return render_template('register.html')
 
-@auth.route('/login', methods=['POST'])
+@auth.route('/login')
 def login():
-    email = request.form.get('email')
-    username = request.form.get('username')
-    password = request.form.get('password')
     form = LoginForm()
     if form.validate_on_submit():
-        name_entered = form.name.data
-        user = User.query.filter_by(username=name_entered).first()
-        login_user(user)
-        flash(f'Welcome {form.username.data}!')
-        return redirect(url_for('.index'))
+        name_entered = form.email.data
+        password_entered = form.password.data
+        user = User.query.filter_by(email=name_entered).first()
+        if user.verify_password(password_entered) == True:
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
     else:
-        flash('Try again')
+        flash('Username or password is invalid')
     return render_template('auth/login.html', form=form)
 
 @auth.route('/logout')
