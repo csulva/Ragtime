7fb052da53aea560daf0da69b1c6aab8792f798d
csulva <chrissulva1@gmail.com>
2021-11-01 17:19:06 -0400

    working on new confirmation emails functions

--- flask-project/files-for-flask-project/app/auth/views.py
+++ flask-project/files-for-flask-project/app/auth/views.py
@@ -68,3 +68,18 @@ def confirm(token):
     else:
         flash('Whoops, that confirmation link either expired or isn\'t valid.')
         return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated \
+            and not current_user.confirmed \
+            and request != 'static' \
+            and request.blueprint != 'auth' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html', user=current_user)
