88324fbe13c2f42ee42e7fcb40c3baa163f2dae4
csulva <chrissulva1@gmail.com>
2021-11-02 22:38:32 -0400

    adding inject permissions

--- flask-project/files-for-flask-project/app/main/__init__.py
+++ flask-project/files-for-flask-project/app/main/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from app.models import Permission
 
 main = Blueprint('main', __name__)
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
 
 from . import errors, views
