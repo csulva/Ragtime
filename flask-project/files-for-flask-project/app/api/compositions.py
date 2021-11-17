e4aa23b905355e75ca325b2f954635504028c2cb
csulva <chrissulva1@gmail.com>
2021-11-17 16:52:34 -0500

    changes to api routes

--- flask-project/files-for-flask-project/app/api/compositions.py
+++ flask-project/files-for-flask-project/app/api/compositions.py
@@ -1,8 +1,9 @@
-from flask import jsonify, request, url_for, g
+from flask import jsonify, request, url_for, g, current_app
 from . import api
 from .decorators import permission_required
 from .. import db
 from ..models import Composition, Permission
+from .errors import forbidden
 
 @api.route('/compositions/', methods=["POST"])
 @permission_required(Permission.PUBLISH)
@@ -17,10 +18,42 @@ def new_composition():
 
 @api.route('/compositions/<int:id>', methods=["GET"])
 def get_composition(id):
-    return jsonify({})
+    composition = Composition.query.get_or_404(id)
+    return jsonify(composition.to_json())
 
-# @api.route('/compositions/')
-# def get_compositions():
-#     compositions = Composition.query.all()
-#     return jsonify({ 'compositions': [composition.to_json()
-#                                       for composition in compositions]})
+@api.route('/compositions/<int:id>', methods=['PUT'])
+@permission_required(Permission.PUBLISH)
+def edit_composition(id):
+    composition = Composition.query.get_or_404(id)
+    if g.current_user != composition.artist and \
+            not g.current_user.can(Permission.ADMIN):
+        return forbidden('Insufficient permissions')
+    import json
+    put_json = json.loads(request.json)
+    composition.release_type = put_json.get('release_type', composition.release_type)
+    composition.title = put_json.get('release_type', composition.title)
+    composition.description = put_json.get('description', composition.description)
+    db.session.add(composition)
+    db.session.commit()
+    return jsonify(composition.to_json())
+
+@api.route('/compositions/')
+def get_compositions():
+    page = request.args.get('page', 1, type=int)
+    pagination = Composition.query.paginate(
+        page,
+        per_page=current_app.config['RAGTIME_COMPS_PER_PAGE'],
+        error_out=False)
+    compositions = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_compositions', page=page-1)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_compositions', page=page+1)
+    return jsonify({
+        'compositions': [composition.to_json() for composition in compositions],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
--- flask-project/files-for-flask-project/app/api/users.py
+++ flask-project/files-for-flask-project/app/api/users.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from . import api
-from ..models import User
+from ..models import Composition, User
 
 @api.route('/users/<int:id>')
 def get_user(id):
@@ -9,8 +9,12 @@ def get_user(id):
 
 @api.route('/users/<int:id>/compositions/', methods=["GET"])
 def get_user_compositions(id):
-    return jsonify({})
+    user = User.query.get_or_404(id)
+    return jsonify({ 'compositions': [composition.to_json()
+                                     for composition in user.compositions]})
 
 @api.route('/users/<int:id>/followed/', methods=["GET"])
 def get_user_followed(id):
-    return jsonify({})
+    user = User.query.get_or_404(id)
+    return jsonify({ 'compositions': [composition.to_json()
+                                     for composition in user.followed_compositions]})
