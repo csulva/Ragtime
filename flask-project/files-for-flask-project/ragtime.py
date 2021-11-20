2124156e7352a3543dd6af794a044b15d4f33898
csulva <chrissulva1@gmail.com>
2021-11-20 18:31:05 -0500

    adding deploy method

--- flask-project/files-for-flask-project/ragtime.py
+++ flask-project/files-for-flask-project/ragtime.py
@@ -2,6 +2,7 @@ from app import create_app, db
 from app.models import Composition, Role, User, Follow
 import os
 from flask_migrate import Migrate
+from flask_migrate import upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -10,5 +11,16 @@ migrate = Migrate(app, db, render_as_batch=True)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, Role=Role, User=User, Composition=Composition, Follow=Follow)
+
+@app.cli.command()
+def deploy():
+    """ Run deployment tasks """
+    # migrate database
+    upgrade()
+
+    Role.insert_roles()
+
+    User.add_self_follows()
+
 
 app.run()
