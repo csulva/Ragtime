51fe2182426a1f6a9a3443941ed53d289f1f5f7f
csulva <chrissulva1@gmail.com>
2022-01-19 00:17:19 -0500

    comments and docstrings

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -5,11 +5,24 @@ import pytest
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 def test_database_insert(new_app):
+    """Tests to see if Users can be created from the database, added to the database
+    and that it can be committed.
+
+    Args:
+        new_app (function): creates an app to test this function with database. See: conftest.py
+    """
     u = User(username='john')
     db.session.add(u)
     db.session.commit()
 
 def test_password_functionality(new_app):
+    """Tests to see if User passwords can be created, added, and committed. Also tests the
+    'verify_password()' function to check that the password created returns True when provided
+    with the correct password because passwords are hashed for security purposes.
+
+    Args:
+        new_app (function): creates an app to test this function with database. See: conftest.py
+    """
     u = User(password='test')
     u1 = User(password='test')
     db.session.add(u)
@@ -21,20 +34,5 @@ def test_password_functionality(new_app):
         assert u.password=='Password is not a readable attribute'
     u.password_hash != u1.password_hash
 
-# Test token creation and verification:
 
-# def generate_confirmation_token(self, expiration_sec=3600):
-#     s = Serializer(current_app.secret_key, expiration_sec)
-#     return s.dumps({'confirm_id': self.id}).decode('utf-8')
 
-# def confirm(self, token):
-#     s = Serializer(current_app.secret_key)
-#     try:
-#         data = s.loads(token.encode('utf-8'))
-#     except:
-#         return False
-#     if data.get('confirm_id') != self.id:
-#         return False
-#     self.confirmed = True
-#     db.session.add(self)
-#     return True
