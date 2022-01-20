db96aa4c7d08349ce11dfec9a8c871a6d85b8366
csulva <chrissulva1@gmail.com>
2022-01-19 22:50:17 -0500

    app factory comments

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_wtf.csrf import CSRFProtect
 
-
+# Class instances from imports
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -16,7 +16,7 @@ mail = Mail()
 moment = Moment()
 csrf = CSRFProtect()
 
-
+# App factory
 def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
@@ -29,7 +29,7 @@ def create_app(config_name='default'):
     bootstrap.init_app(app)
     db.init_app(app)
 
-
+    # Registering blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -39,6 +39,7 @@ def create_app(config_name='default'):
     from .api import api as api_blueprint
     app.register_blueprint(api_blueprint)
 
+    # Added security
     if app.config['HTTPS_REDIRECT']:
         from flask_talisman import Talisman
         Talisman(app, content_security_policy={
