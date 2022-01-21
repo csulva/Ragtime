14022f7091afd52dd8a4bcb5396888405a8d2377
csulva <chrissulva1@gmail.com>
2022-01-20 23:01:12 -0500

    docstrings for the blueprint

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,6 +5,9 @@ main = Blueprint('main', __name__)
 
 @main.app_context_processor
 def inject_permissions():
+    """Access the Permission database table/class without needing to call it every time
+    you need to pass it into functions.
+    """
     return dict(Permission=Permission)
 
 from . import errors, views
