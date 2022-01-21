c2384a840d54c3befb217bd5c7876a567fc839bd
csulva <chrissulva1@gmail.com>
2022-01-21 14:26:11 -0500

    docstrings for api compositions

--- app/api/compositions.py
+++ app/api/compositions.py
@@ -8,6 +8,12 @@ from .errors import forbidden
 @api.route('/compositions/', methods=["POST"])
 @permission_required(Permission.PUBLISH)
 def new_composition():
+    """Posts new composition and creates API data for it
+
+    Returns:
+        .json: json data for the created composition
+    """
+    # Create composition through the API
     composition = Composition.from_json(request.json)
     composition.artist = g.current_user
     db.session.add(composition)
@@ -18,18 +24,37 @@ def new_composition():
 
 @api.route('/compositions/<int:id>', methods=["GET"])
 def get_composition(id):
+    """Gets API for a composition given its ID
+
+    Args:
+        id (int): The ID for the composition
+
+    Returns:
+        .json: json data for the composition
+    """
     composition = Composition.query.get_or_404(id)
     return jsonify(composition.to_json())
 
 @api.route('/compositions/<int:id>', methods=['PUT'])
 @permission_required(Permission.PUBLISH)
 def edit_composition(id):
+    """Edit a given composition in a PUT request
+
+    Args:
+        id (int): the ID for the composition
+
+    Returns:
+        .json: the edited composition in the API
+    """
+    # Finds the composition with the ID
     composition = Composition.query.get_or_404(id)
+    # Only the user can edit their own composition, or must be an administrator
     if g.current_user != composition.artist and \
             not g.current_user.can(Permission.ADMIN):
         return forbidden('Insufficient permissions')
     import json
     put_json = json.loads(request.json)
+    # Makes changes to the composition
     composition.release_type = put_json.get('release_type', composition.release_type)
     composition.title = put_json.get('release_type', composition.title)
     composition.description = put_json.get('description', composition.description)
@@ -39,11 +64,15 @@ def edit_composition(id):
 
 @api.route('/compositions/')
 def get_compositions():
+    """Returns all compositions in API
+    """
+    # Paginate the compositions
     page = request.args.get('page', 1, type=int)
     pagination = Composition.query.paginate(
         page,
         per_page=current_app.config['RAGTIME_COMPS_PER_PAGE'],
         error_out=False)
+    # Converts to list
     compositions = pagination.items
     prev = None
     if pagination.has_prev:
