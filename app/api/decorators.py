e56acd1656b8659be771a1d251be064d67dca8cd
csulva <chrissulva1@gmail.com>
2022-01-21 14:26:32 -0500

    docstring

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -3,6 +3,11 @@ from functools import wraps
 from .errors import forbidden
 
 def permission_required(permission):
+    """Decorator function used for functions where a certain permisson (given) is required
+
+    Args:
+        permission (int): Permission int described in models.py
+    """
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
