c148475027c039c9fa9e84e15d055aa054ef30ca
csulva <chrissulva1@gmail.com>
2022-01-21 15:07:06 -0500

    docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from flask import current_app
 
 @pytest.fixture(scope='module')
 def new_app():
+    """Tests that test database works when creating an app in 'testing' configuration
+    """
     app = create_app('testing')
     assert 'data-test.sqlite' in app.config['SQLALCHEMY_DATABASE_URI']
     test_client = app.test_client()
