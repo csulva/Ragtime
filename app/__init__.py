c2acb9861024fa333bb440baccce5b0dc250c22c
csulva <chrissulva1@gmail.com>
2022-01-24 18:46:54 -0500

    working on flask moment

--- app/__init__.py
+++ app/__init__.py
@@ -23,10 +23,11 @@ def create_app(config_name='default'):
     config[config_name].init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    moment.init_app(app)
     csrf.init_app(app)
 
     bootstrap.init_app(app)
+    moment.init_app(app)
+
     db.init_app(app)
 
     # Registering blueprints
